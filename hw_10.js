094060c866e3b9970c02263fe66bc63dea5a670d
Nataliia Berezovska <berezow713@gmail.com>
2023-10-17 20:22:08 +0200

    modified homework 2nd task

--- hw_10.js
+++ hw_10.js
@@ -23,20 +23,29 @@ console.log(Math.sqrt(16));    // 4
 перетворіть її на число
 перетворіть її на булеве значення */
 
-let a = 5; //число
+/*let a = 5; //число
 let b = "5"; // string
 let c = a + b; 
 //alert(typeof(c))// виведе string
-console.log(c);
+console.log(c);*/
+
+
+const a = 170;
+ console.log(a.toString());
 
 let participants = +"5";
 console.log(participants);
 
 
-const age = 20;
-if (age) {
-    console.log('true')
-};
+console.log(Boolean(5)); //  true
+//or
+console.log(!!5); //  true
+
+
+
+
+
+
 
 
 
