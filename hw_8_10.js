bdcb062293134f6d6ff4e3bcfdea3e1835de2405
Nataliia Berezovska <berezow713@gmail.com>
2023-11-25 14:17:13 +0100

    homework 8

---
+++ hw_8_10.js
@@ -0,0 +1,19 @@
+'use strict';
+
+//— для елементу з текстом 'Навігація по DOM дереву'
+let elementWithId = document.querySelector('#headerTwo');
+
+//— для першого елементу <section>
+let firstChildOfSection = document.querySelector('.firstSection').firstElementChild;
+
+//для елементу списку з текстом 'Пункт 5'
+let items = document.querySelectorAll('ul li');
+
+let desiredItem = items[4];
+
+// для елементу з класом 'hatredLevelBlock'
+let hatredLevelBlock = document.querySelector('div.hatredLevelBlock');
+
+
+
+
